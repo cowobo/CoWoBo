cc5160c71de229eab690d69414cff43c56146af6
mike <mike@localhost>
2013-01-15 22:07:37 +0200

    Set and get cookie, and set global $lang in L10n class

--- wp-content/plugins/cowobo/lib/class-cowobo-l10n.php
+++ wp-content/plugins/cowobo/lib/class-cowobo-l10n.php
@@ -3,9 +3,25 @@
 if (!defined('ABSPATH'))
     exit;
 
+/**
+ * L10n
+ *
+ * Manual localization
+ */
 class CoWoBo_Localization
 {
-    //return translated content if available
+
+    public function __construct() {
+        global $cowobo, $lang;
+
+        //Update language session
+        if ( $lang = $cowobo->query->lang ) $this->set_lang_cookie ( $lang );
+        else $lang = $this->get_lang_cookie();
+    }
+
+    /**
+     * Return translated content if available
+     */
     public function the_content($postid) {
         global $lang;
         if ($translated = get_post_meta($postid, 'content-'.$lang, true)) {
@@ -14,7 +30,9 @@ class CoWoBo_Localization
         return get_the_content($postid);
     }
 
-    //return translated title if available
+    /**
+     * Return translated title if available
+     */
     public function the_title($postid) {
         global $lang;
         if ($translated = get_post_meta($postid, 'title-'.$lang, true)) {
@@ -23,12 +41,32 @@ class CoWoBo_Localization
         return get_the_title($postid);
     }
 
-    //add translated versions of post as custom fields
+    /**
+     * Add translated versions of post as custom fields
+     */
     public function correct_translation() {
-        global $lang; global $post;
+        global $lang, $post;
         update_post_meta($post->ID, 'title-'.$lang, $_POST['title-'.$lang]);
         update_post_meta($post->ID, 'content-'.$lang, $_POST['content-'.$lang]);
         return $notices;
+    }
+
+    /**
+     * Set language cookie using the cookie duration for the remembered login cookie
+     *
+     * @param string $lang
+     */
+    public function set_lang_cookie ( $lang = '' ) {
+        global $cowobo;
+
+        if ( empty ( $lang ) ) return;
+        $cowobo->query->set_cookie( 'cowobo_lang', $lang );
+
+    }
+
+    public function get_lang_cookie() {
+        global $cowobo;
+        return $cowobo->query->get_cookie('cowobo_lang');
     }
 
 }
