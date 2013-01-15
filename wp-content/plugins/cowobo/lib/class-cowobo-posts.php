138f0ed3a8e7684cae1c644d8f11a20769bf334c
mike <mike@localhost>
2013-01-15 19:08:14 +0200

    Somewhat pimped save_post and added get_category

--- wp-content/plugins/cowobo/lib/class-cowobo-posts.php
+++ wp-content/plugins/cowobo/lib/class-cowobo-posts.php
@@ -47,156 +47,169 @@ class CoWoBo_Posts
             );
         }
     }
-}
 
-
-//Save post with new data
-function cwb_save_post(){
-	global $related; global $post; global $social;
-
-	//store all data
-	$postid = $_POST['post_ID'];
-
-	$post_title  = (isset($_POST['post_title'])) ? trim(strip_tags($_POST['post_title'])) : null;
-	$post_content = ( isset($_POST['post_content']) ) ? trim($_POST['post_content']) : null;
-	$tags  = (isset($_POST['tags'])) ? trim(strip_tags($_POST['tags'])) : null;
-	$oldcity = get_post_meta($postid, 'cityid', true);
-	$oldslug = $post->post_name;
-	$involvement = $_POST['involvement'];
-	$newslug = sanitize_title($post_title);
-	$postcat = cwob_get_category($postid);
-	$author = true;
-
-	//check if post is created from within another post
-	if($postid != $post->ID) $linkedid = $post->ID;
-
-
-	//if the user is not involved don't link it to their profile
-	if($involvement == 'none'):
-		$related->delete_relations($postid, $social->post_id); //existing posts
-		$linkedid = false;
-	else:
-		$linkedid = $social->profile_id;
-	endif;
-
-	//check if title filled correctly
-	if ($post_title == '') $postmsg['post_title'] = 'You forgot to add one.';
-
-	//check if the user entered all text in english
-	if(!$_POST['confirmenglish'])  $postmsg['confirmenglish'] = 'Please check if all text is in English and check the checbox below';
-
-	//update all the custom fields
-	foreach ($_POST as $key => $value) :
-		if($value != ''):
-			delete_post_meta($postid, $key);
-			if(strpos($key,'-checked')== true):
-				foreach ($value as $newval):
-					add_post_meta($postid, $key, $newval);
-				endforeach;
-			else:
-				add_post_meta($postid, $key, $value);
-			endif;
-		endif;
-	endforeach;
-
-	//if its a new location post geocode its location
-	if($postcat->slug == 'location'):
-		if($countryid = $_POST['country']):
-			$tagarray = array($countryid);
-			if($latlng = cwb_geocode($post_title.', '.$country)):
-				$coordinates = $latlng['lat'].','.$latlng['lng'];
-				$citypost = get_posts('meta_key=coordinates&meta_value='.$coordinates);
-				//check if coordinates have already been added (avoids international spelling differences)
-				if($citypost && $citypost[0]->ID != $postid):
-					$postmsg['post_title'] = 'The location you are trying to add already exists';
-				else:
-					add_post_meta($postid, 'coordinates', $coordinates);
-				endif;
-				if(!empty($linkedid)): $related->create_relations($postid, array($linkedid)); endif;
-			else:
-				$postmsg['post_title'] = 'We could not find that city. Check your spelling or internet connection.';
-			endif;
-		else:
-			$postmsg['country'] = 'Please select a country';
-		endif;
-	endif;
-
-	//if post contains a location create or link to that location post
-	if($city = $_POST['city']):
-		if($city != get_post_meta($postid, 'cityid', true)):
-			if($countryid = $_POST['country']):
-				$countrycat = get_category($countryid);
-				if($latlng = cwb_geocode($city.', '.$countrycat->name)):
-					$coordinates = $latlng['lat'].','.$latlng['lng'];
-					$citypost = get_posts('meta_key=coordinates&meta_value='.$coordinates);
-					//check if coordinates have already been added (avoids international spelling differences)
-					if($citypost):
-						$cityid = $citypost[0]->ID;
-					else:
-						//todo use returned geocoding city name
-						$cityid = wp_insert_post(array('post_title'=>$city, 'post_category'=>array($countryid), 'post_status'=>'Publish'));
-						add_post_meta($cityid, 'coordinates', $coordinates);
-					endif;
-					$related->delete_relations($postid, $oldcity);
-					$related->create_relations($postid, array($cityid));
-					add_post_meta($postid, 'cityid', $cityid);  //save ID to check city next time
-					update_post_meta($postid, 'coordinates', $coordinates);
-				else:
-					$postmsg['location'] = 'We could not find that city. Check your spelling or internet connection.';
-				endif;
-			else:
-				$postmsg['location'] = 'Please select a country';
-			endif;
-		endif;
-		add_post_meta($postid, $key, $value);
-	endif;
-
-	//get ids for each tag and create them if they dont already exist
-	if ($tags != ''):
-		foreach(explode(',', $tags) as $tag):
-			$tagid = term_exists(trim($tag), 'category', $postcat->term_id);
-			if(!$tagid) $tagid = wp_insert_term(trim($tag), 'category', array('parent'=> $postcat->term_id));
-			$tagarray[] = $tagid['term_id'];
-		endforeach;
-		$tagarray = array_map('intval', $tagarray);
-	    $tagarray = array_unique($tagarray);
-	elseif($postcat->slug != 'location'):
-		 $postmsg['tags'] = 'You must add atleast one.';
-	endif;
-
-	//handle images
     /**
-     * @todo check for malicious code in jpg?
+     * Save post with new data
+     * @todo This is one beast of a method - can we make some subroutines?
      */
-	for ($x=0; $x<5; $x++):
-		$imgid = $_POST['imgid'.$x];
-		$file = $_FILES['file'.$x]['name'];
-		$videocheck = explode("?v=", $_POST['caption'.$x]);
-		//delete image if selected or being replaced by something else
-		if($_POST['delete'.$x] or !empty($file) or !empty($videocheck[1])):
-			wp_delete_attachment($imgid, true);
-			delete_post_meta($postid, 'imgid'.$x);
-		endif;
-		//add new image
-		if(!empty($file)):
-			$imgid = cwb_insert_attachment('file'.$x, $postid);
-			update_post_meta($postid, 'imgid'.$x, $imgid);
-		endif;
-	endfor;
+    public function save_post(){
+        global $related, $post, $social, $cowobo, $profile_id;
 
-	//update draft post
-	$postdata = array('ID' => $postid, 'post_title' => $post_title, 'post_content' => $post_content, 'post_status' => 'draft', 'post_category' => $tagarray);
-	wp_update_post($postdata);
+        //store all data
+        $postid = $cowobo->query->post_ID;
 
-	// if there are no errors publish post, add links, and show thanks for saving message
-	if(empty($postmsg)):
-		wp_update_post( array('ID' => $postid,'post_status' => 'publish', 'post_name' =>$newslug));
-		if(!empty($linkedid)) $related->create_relations($postid, array($linkedid));
-		$postmsg = 'saved';
-	endif;
+        $post_title  = ( $cowobo->query->post_title ) ? trim(strip_tags( $cowobo->query->post_title ) ) : null;
+        $post_content = ( $cowobo->query->post_content ) ? trim( $cowobo->query->post_content ) : null;
+        $tags  = ( $cowobo->query->tags ) ? trim(strip_tags( $cowobo->query->tags ) ) : null;
+        $oldcity = get_post_meta( $postid, 'cityid', true );
+        //$oldslug = $post->post_name;
+        $involvement = $cowobo->query->involvement;
+        $newslug = sanitize_title($post_title);
+        $postcat = $this->get_category($postid);
 
-	return $postmsg;
+        //check if post is created from within another post
+        if($postid != $post->ID) $linkedid = $post->ID;
+
+        //if the user is not involved don't link it to their profile
+        if($involvement == 'none') {
+            $related->delete_relations($postid, $profile_id); //existing posts
+            $linkedid = false;
+        } else {
+            $linkedid = $profile_id;
+        }
+
+        //check if title filled correctly
+        if ($post_title == '') $postmsg['post_title'] = 'You forgot to add one.';
+
+        //check if the user entered all text in english
+        if(!$cowobo->query->confirmenglish)  $postmsg['confirmenglish'] = 'Please check if all text is in English and check the checbox below';
+
+        /**
+         * update all the custom fields
+         * @todo !! Iterate over POST more conciously
+         */
+        foreach ($_POST as $key => $value) {
+            if( empty ( $value ) ) continue;
+
+            delete_post_meta($postid, $key);
+            if(strpos($key,'-checked')== true) {
+                foreach ($value as $newval) {
+                    add_post_meta($postid, $key, $newval);
+                }
+            }else {
+                add_post_meta($postid, $key, $value);
+            }
+        }
+
+        //if its a new location post geocode its location
+        if( $postcat->slug == 'location' ) {
+            if( $countryid = $cowobo->query->country ) {
+                $tagarray = array( $countryid );
+                if($latlng = cwb_geocode( $post_title.', '.$country ) ) {
+                    $coordinates = $latlng['lat'].','.$latlng['lng'];
+                    $citypost = get_posts('meta_key=coordinates&meta_value='.$coordinates);
+                    //check if coordinates have already been added (avoids international spelling differences)
+                    if($citypost && $citypost[0]->ID != $postid) {
+                        $postmsg['post_title'] = 'The location you are trying to add already exists';
+                    } else {
+                        add_post_meta($postid, 'coordinates', $coordinates);
+                    }
+                    if( ! empty( $linkedid ) ) $related->create_relations($postid, array($linkedid));
+                } else {
+                    $postmsg['post_title'] = 'We could not find that city. Check your spelling or internet connection.';
+                }
+            } else {
+                $postmsg['country'] = 'Please select a country';
+            }
+        }
+
+        //if post contains a location create or link to that location post
+        if( $city = $cowobo->query->city ) {
+            if($city != $oldcity ) {
+                if($countryid = $_POST['country']) {
+                    $countrycat = get_category($countryid);
+                    if($latlng = cwb_geocode($city.', '.$countrycat->name)):
+                        $coordinates = $latlng['lat'].','.$latlng['lng'];
+                        $citypost = get_posts('meta_key=coordinates&meta_value='.$coordinates);
+                        //check if coordinates have already been added (avoids international spelling differences)
+                        if($citypost):
+                            $cityid = $citypost[0]->ID;
+                        else:
+                            // @todo use returned geocoding city name
+                            $cityid = wp_insert_post(array('post_title'=>$city, 'post_category'=>array($countryid), 'post_status'=>'Publish'));
+                            add_post_meta($cityid, 'coordinates', $coordinates);
+                        endif;
+                        $related->delete_relations($postid, $oldcity);
+                        $related->create_relations($postid, array($cityid));
+                        add_post_meta($postid, 'cityid', $cityid);  //save ID to check city next time
+                        update_post_meta($postid, 'coordinates', $coordinates);
+                    else:
+                        $postmsg['location'] = 'We could not find that city. Check your spelling or internet connection.';
+                    endif;
+                } else {
+                    $postmsg['location'] = 'Please select a country';
+                }
+            }
+        }
+
+        //get ids for each tag and create them if they dont already exist
+        if ( ! empty ( $tags ) ) {
+            foreach(explode(',', $tags) as $tag) {
+                $tagid = term_exists(trim($tag), 'category', $postcat->term_id);
+                if(!$tagid) $tagid = wp_insert_term(trim($tag), 'category', array('parent'=> $postcat->term_id));
+                $tagarray[] = $tagid['term_id'];
+            }
+            $tagarray = array_map('intval', $tagarray);
+            $tagarray = array_unique($tagarray);
+        } elseif($postcat->slug != 'location') {
+             $postmsg['tags'] = 'You must add atleast one.';
+        }
+
+        //handle images
+        /**
+         * @todo check for malicious code in jpg?
+         */
+        for ($x=0; $x<5; $x++):
+            $imgid = $_POST['imgid'.$x];
+            $file = $_FILES['file'.$x]['name'];
+            $videocheck = explode("?v=", $_POST['caption'.$x]);
+            //delete image if selected or being replaced by something else
+            if($_POST['delete'.$x] or !empty($file) or !empty($videocheck[1])):
+                wp_delete_attachment($imgid, true);
+                delete_post_meta($postid, 'imgid'.$x);
+            endif;
+            //add new image
+            if(!empty($file)):
+                $imgid = cwb_insert_attachment('file'.$x, $postid);
+                update_post_meta($postid, 'imgid'.$x, $imgid);
+            endif;
+        endfor;
+
+        //update draft post
+        $postdata = array('ID' => $postid, 'post_title' => $post_title, 'post_content' => $post_content, 'post_status' => 'draft', 'post_category' => $tagarray);
+        wp_update_post($postdata);
+
+        // if there are no errors publish post, add links, and show thanks for saving message
+        if(empty($postmsg)):
+            wp_update_post( array('ID' => $postid,'post_status' => 'publish', 'post_name' =>$newslug));
+            if(!empty($linkedid)) $related->create_relations($postid, array($linkedid));
+            $postmsg = 'saved';
+        endif;
+
+        return $postmsg;
+    }
+
+    /**
+     * Get primal category of post
+     */
+    public function get_category($postid) {
+        $cat = get_the_category($postid);
+        $ancestors = get_ancestors($cat[0]->term_id,'category');
+        if (empty($ancestors)) return $cat[0];
+        return get_category(array_pop($ancestors));
+    }
 }
+
 
 //insert and resize uploaded attachments
 function cwb_insert_attachment($file_handler,$post_id,$setthumb='false') {
@@ -213,15 +226,6 @@ function cwb_insert_attachment($file_handler,$post_id,$setthumb='false') {
 function cwb_link_post(){
 	global $related; global $post;
 	$related->create_relations($post->ID, array($_POST['linkto']));
-}
-
-
-//Get primal category of post
-function cwob_get_category($postid) {
-	$cat = get_the_category($postid);
-	$ancestors = get_ancestors($cat[0]->term_id,'category');
-	if (empty($ancestors)) return $cat[0];
-	return get_category(array_pop($ancestors));
 }
 
 //Return time passed since publish date
