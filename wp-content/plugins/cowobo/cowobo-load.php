9db0d0ddd61cddacc5bd3d7bc839037a8969d5cf
mike <mike@localhost>
2013-01-15 15:46:14 +0200

    Initialize CoWoBo_Query as $cowobo->query and use it for the loadcheck

--- wp-content/plugins/cowobo/cowobo-load.php
+++ wp-content/plugins/cowobo/cowobo-load.php
@@ -26,13 +26,28 @@ define('COWOBO_PLUGIN_VERSION', '0.1');
  */
 define('COWOBO_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('COWOBO_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('COWOBO_PLUGIN_LIB', COWOBO_PLUGIN_DIR . 'lib/' );
 //define('COWOBO_PLUGIN_TEMPLATES_DIR', COWOBO_PLUGIN_DIR . 'templates/');
 //define('COWOBO_PLUGIN_TEMPLATES_URL', COWOBO_PLUGIN_URL . 'templates/');
 //define('COWOBO_PLUGIN_INC_URL', COWOBO_PLUGIN_TEMPLATES_URL . '_inc/');
 
+/**
+ * Requires and includes
+ *
+ * @since 0.1
+ */
+require_once ( COWOBO_PLUGIN_LIB . 'query');
+
 if (!class_exists('CoWoBo')) :
 
     class CoWoBo    {
+
+        /**
+         * Var to contain the query class, for easy access to query vars
+         *
+         * @var type
+         */
+        public $query;
 
         /**
          * Creates an instance of the CoWoBo class
@@ -58,16 +73,35 @@ if (!class_exists('CoWoBo')) :
          * @since 0.1
          */
         public function __construct() {
+            $this->query = new CoWoBo_Query;
 
+            $this->controller();
         }
 
-        /**
-         * PHP4
-         *
-         * @since 0.1
+            /**
+             * PHP4
+             *
+             * @since 0.1
              */
-        public function CoWoBo() {
-            $this->__construct();
+            public function CoWoBo() {
+                $this->__construct();
+            }
+
+        private function controller() {
+            $query = &$this->query;
+
+            if( $query->confirm ) cwb_create_user();
+            elseif( $query->userpw && !$_POST['user']) cwb_login_user();
+            elseif( $query->sort ) cwb_filter_feed();
+            elseif( $query->showall ) cwb_related_feed();
+            elseif( $query->delete ) cwb_delete_post();
+            elseif( $query->new ) $GLOBALS['postid'] = cwb_create_post();
+            elseif( $query->requesttype ) $notices = cwb_edit_request();
+            elseif( $query->correctlang ) $notices = cwb_correct_translation();
+            elseif( $query->emailtext && ! $query->user ) $notices = cwb_send_email();
+            elseif( $query->linkto ) $notices = cwb_link_post();
+            elseif( $query->commentid ) wp_delete_comment($_POST['commentid']);
+            elseif( $query->post_ID ) $GLOBALS['postmsg'] = cwb_save_post();
         }
 
 
