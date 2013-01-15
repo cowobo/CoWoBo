9baf28225f550499732ec791252ffc94acdcfa16
mike <mike@localhost>
2013-01-15 19:38:17 +0200

    Reintroduce a function in functions.php! (cbw_time_passed(); )

--- wp-content/themes/cowobo/functions.php
+++ wp-content/themes/cowobo/functions.php
@@ -384,4 +384,28 @@ $langnames = array(
 	'zh-CN' => array('中国', '欢迎到编码器无国界', '继续在中国', '网页翻译。'),
 );
 
-?>
+// Utility functions
+
+/**
+ * Return time passed since publish date
+ */
+function cwb_time_passed($timestamp){
+    $timestamp = (int) $timestamp;
+    $current_time = time();
+    $diff = $current_time - $timestamp;
+    $intervals = array ('day' => 86400, 'hour' => 3600, 'minute'=> 60);
+    //now we just find the difference
+    if ($diff == 0) return 'just now &nbsp;';
+    if ($diff < $intervals['hour']){
+        $diff = floor($diff/$intervals['minute']);
+        return $diff == 1 ? $diff . ' min ago' : $diff . ' mins ago';
+    }
+    if ($diff >= $intervals['hour'] && $diff < $intervals['day']){
+        $diff = floor($diff/$intervals['hour']);
+        return $diff == 1 ? $diff . ' hour ago' : $diff . ' hours ago';
+    }
+    if ($diff >= $intervals['day']){
+        $diff = floor($diff/$intervals['day']);
+        return $diff == 1 ? $diff . ' day ago' : $diff . ' days ago';
+    }
+}
