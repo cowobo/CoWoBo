58c88b8016342878c0928fd1067a8ad670eff3dd
Mike <mike@trenvo.com>
2013-01-18 18:50:48 +0200

    Add author to post when adding new post

--- wp-content/plugins/cowobo/lib/class-cowobo-posts.php
+++ wp-content/plugins/cowobo/lib/class-cowobo-posts.php
@@ -48,8 +48,8 @@ class CoWoBo_Posts
 
         if ( ! $postid ) {
             $postid = $GLOBALS['newpostid'] = wp_insert_post( array('post_name' =>$newslug, 'post_category' => array ( get_cat_ID( $cowobo->query->new ) ), 'post_content' => " " ) );
+            add_post_meta( $postid, 'author', $profile_id);
         }
-
 
         //check if post is created from within another post
         if($postid != $post->ID) $linkedid = $post->ID;
