aaa4c1449d78fdb07c1050d9ede8d464e79c88ac
Mike <mike@trenvo.com>
2013-01-19 15:26:01 +0200

    Added the record_post_edit method to cowobo_update_post

--- wp-content/plugins/cowobo/lib/class-cowobo-buddypress.php
+++ wp-content/plugins/cowobo/lib/class-cowobo-buddypress.php
@@ -40,6 +40,10 @@ class CoWoBo_BuddyPress
 
         // Disable favorites
         add_filter ( 'bp_activity_can_favorite' ,'__return_false' );
+
+        // Add updated post activity
+        add_filter ( 'cowobo_post_updated', array ( &$this, 'record_post_edited' ), 10, 3 );
+
     }
 
     public function cowobo_user_domain( $domain, $user_id ) {
@@ -128,6 +132,68 @@ class CoWoBo_BuddyPress
         } else {
             return $subject;
         }
+    }
+
+    function record_post_edited( $post_id, $post_title ) {
+        global $bp, $wpdb;
+
+        $post_id = (int) $post_id;
+        $blog_id = (int) $wpdb->blogid;
+        $user_id = get_current_user_id();
+
+        // If blog is not trackable, do not record the activity.
+        if ( ! bp_blogs_is_blog_trackable( $blog_id, $user_id ) )
+            return false;
+
+        if ( !$user_id )
+            return false;
+
+        $is_blog_public = apply_filters( 'bp_is_blog_public', (int)get_blog_option( $blog_id, 'blog_public' ) );
+
+        if ( $is_blog_public || !is_multisite() ) {
+
+            // Record this in activity streams
+            $post_permalink   = get_permalink( $post_id );
+
+            if ( is_multisite() )
+                $activity_action  = sprintf( __( '%1$s updated the post %2$s, on the site %3$s', 'cowobo' ), bp_core_get_userlink( (int) $user_id ), '<a href="' . $post_permalink . '">' . $post_title . '</a>', '<a href="' . get_blog_option( $blog_id, 'home' ) . '">' . get_blog_option( $blog_id, 'blogname' ) . '</a>' );
+            else
+                $activity_action  = sprintf( __( '%1$s updated the post %2$s', 'cowobo' ), bp_core_get_userlink( (int) $user_id ), '<a href="' . $post_permalink . '">' . $post_title . '</a>' );
+
+            // Make sure there's not an existing entry for this post (prevent bumping)
+            if ( bp_is_active( 'activity' ) ) {
+                $existing = bp_activity_get( array(
+                    'filter' => array(
+                        'user_id'      => (int) $user_id,
+                        'action'       => 'updated_blog_post',
+                        'primary_id'   => $blog_id,
+                        'secondary_id' => $post_id,
+                    )
+                ) );
+
+                if ( !empty( $existing['activities'] ) ) {
+                    return;
+                }
+            }
+
+            $activity_content = "";
+
+            bp_blogs_record_activity( array(
+                'user_id'           => (int) $user_id,
+                'action'            => apply_filters( 'bp_blogs_activity_updated_post_action',       $activity_action,  $post_title, $post_permalink ),
+                'content'           => apply_filters( 'bp_blogs_activity_updated_post_content',      $activity_content, $post_title, $post_permalink ),
+                'primary_link'      => apply_filters( 'bp_blogs_activity_updated_post_primary_link', $post_permalink,   $post_id               ),
+                'type'              => 'updated_blog_post',
+                'item_id'           => $blog_id,
+                'secondary_item_id' => $post_id,
+                'recorded_time'     => current_time ( 'mysql', 1 )
+            ));
+        }
+
+        // Update the blogs last activity
+        bp_blogs_update_blogmeta( $blog_id, 'last_activity', bp_core_current_time() );
+
+        do_action( 'bp_blogs_updated_blog_post', $post_id, $post_title, $user_id );
     }
 
 }
