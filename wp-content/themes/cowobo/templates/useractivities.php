014c7131fb62cd4769abb434ebbee3272aa12383
Mike <mike@trenvo.com>
2013-01-19 16:39:57 +0200

    Differentiate between mentions and activities

--- wp-content/themes/cowobo/templates/useractivities.php
+++ wp-content/themes/cowobo/templates/useractivities.php
@@ -3,15 +3,13 @@ global $cowobo;
 
 echo "<div id='buddypress'>";
 
-    echo "<div class='tab'>";
-        echo "<h3>Quick Update</h3>";
-        if ( ! $cowobo->users->is_current_user_profile() ) {
-            $_GET['r'] = $cowobo->users->displayed_user->user_nicename;
-        }
-
-        bp_get_template_part( 'activity/post-form' );
-    echo "</div>";
-
+    if ( $cowobo->users->is_current_user_profile() ) {
+        echo "<div class='tab'>";
+            echo "<h3>Quick Update</h3>";
+            $_GET['r'] = null;
+            bp_get_template_part( 'activity/post-form' );
+        echo "</div>";
+    }
 
     echo "<div class='tab activity'>";
         echo "<h3>Activities</h3>";
@@ -19,6 +17,13 @@ echo "<div id='buddypress'>";
         bp_get_template_part( 'activity/activity-loop' );
     echo "</div>";
 
+    if ( ! $cowobo->users->is_current_user_profile() ) {
+        echo "<div class='tab'>";
+            echo "<h3>Leave a message</h3>";
+            $_GET['r'] = $cowobo->users->displayed_user->user_nicename;
+            bp_get_template_part( 'activity/post-form' );
+        echo "</div>";
+    }
     echo "<div class='tab activity'>";
         echo "<h3>Mentions</h3>";
         $cowobo->buddypress->query_filter = 'mentions';
