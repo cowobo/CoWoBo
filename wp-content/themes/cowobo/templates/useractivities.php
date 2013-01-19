adfbce5d4688f38001272a887435188e358eb115
Mike <mike@trenvo.com>
2013-01-19 10:57:48 +0200

    Add useractivities template

---
+++ wp-content/themes/cowobo/templates/useractivities.php
@@ -0,0 +1,29 @@
+<?php
+global $cowobo;
+
+echo "<div id='buddypress'>";
+
+    echo "<div class='tab'>";
+        echo "<h3>Quick Update</h3>";
+        if ( ! $cowobo->users->is_current_user_profile() ) {
+            $_GET['r'] = $cowobo->users->displayed_user->user_nicename;
+        }
+
+        bp_get_template_part( 'activity/post-form' );
+    echo "</div>";
+
+
+    echo "<div class='tab activity'>";
+        echo "<h3>Activities</h3>";
+        $cowobo->buddypress->query_filter = 'user';
+        bp_get_template_part( 'activity/activity-loop' );
+    echo "</div>";
+
+    echo "<div class='tab activity'>";
+        echo "<h3>Mentions</h3>";
+        $cowobo->buddypress->query_filter = 'mentions';
+        bp_get_template_part( 'activity/activity-loop' );
+    echo "</div>";
+
+
+echo "</div>";
