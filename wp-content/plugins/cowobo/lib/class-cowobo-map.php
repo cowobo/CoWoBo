0cd9c56473c0da68dc949c5c09900d70d833fbf4
codingangel <bsieders@gmail.com>
2013-01-22 10:37:07 +0200

    Updated map so it can load local stored tiles

--- wp-content/plugins/cowobo/lib/class-cowobo-map.php
+++ wp-content/plugins/cowobo/lib/class-cowobo-map.php
@@ -35,19 +35,19 @@
 global $offset, $mapdata, $cowobo;
 
 //define the center in pixels of google maps
-$offset = 268435456;
 
 function LonToX($lon) {
-	$offset = 268435456;
+	$offset = 2000;
 	$radius = $offset / pi();
 	return round($offset + $radius * $lon * pi() / 180);
 }
 
 function LatToY($lat) {
-	$offset = 268435456;
+	$offset = 2000;
 	$radius = $offset / pi();
 	return round($offset - $radius * log((1 + sin($lat * pi() / 180)) / (1 - sin($lat * pi() / 180))) / 2);
 }
+
 
 function XToLon($x) {
 	$offset = 268435456;
@@ -75,6 +75,7 @@ function adjustLatByPx($lat, $amount, $zoom) {
 	return $newlat;
 }
 
+
 //check if location entered exists
 function cwb_geocode($address) {
 	$string = str_replace (" ", "+", urlencode($address));
@@ -92,136 +93,62 @@ function cwb_geocode($address) {
 	return $latlng;
 }
 
+
+
 function cwb_loadmap() {
 	global $cowobo, $post;
 	$linkedmarkers = array();
-    $postid = 0;
-
-	//setup default map and tile size
-	$data = array('lat'=> '0', 'lng'=>'40', 'zoom'=>3, 'type'=>'sat', 'path' => '', 'type' => 'sat' );
-	$xmid = 500; $ymid = 250;
-	if( $cowobo->query->post_ID ) $postid = $cowobo->query->post_ID;
-	elseif(is_single()) $postid = $post->ID;
+	
+	$data = array('lat'=> '20', 'lng'=>'0', 'zoom'=>1);
 
 	//get coordinates if specified in url or post
-	$postcoordinates = get_post_meta($postid, 'coordinates', true);
-
-	if($cowobo->query->center) $newcenter = $cowobo->query->center;
-	else $newcenter = $postcoordinates;
-	if($cowobo->query->keywords):
-		$geocode = cwb_geocode($cowobo->query->keywords);
-		$data['lat'] = $geocode['lat'];
-		$data['lng'] = $geocode['lng'];
-	elseif($newcenter):
-		$postcoordinates = explode(',',$newcenter);
-		$data['lat'] = $postcoordinates['0'];
-		$data['lng'] = $postcoordinates['1'];
+	if(is_single()):
+		if( $cowobo->query->post_ID ) $postid = $cowobo->query->post_ID;
+		else $postid = $post->ID;
+		$postcoordinates = get_post_meta($postid, 'coordinates', true);
+		$tileurl = get_bloginfo('template_url').'/images/maps/zoom_1.jpg';
+		echo $cowobo->posts->loadgallery($postid);
 	endif;
-
-	//get zoomlevel if specified in url or post
-	if($cowobo->query->zoom) $newzoom = $cowobo->query->zoom;
-	else $newzoom = get_post_meta($postid, 'zoom', true);
-	if($newzoom) $data['zoom'] = $newzoom;
-	elseif($postcoordinates && $cowobo->query->action != 'editpost' && ! $cowobo->query->new ) $data['zoom'] = 9;
-
-	//check if post has path
-	//if($postpath = get_post_meta($postid, 'encpath', true)) $path = '&shapeformat=cmp&shape='.$postpath;
-
-	//get map type if specified and adjust for different tile providers
-	if( $cowobo->query->maptype ) $data['type'] = $cowobo->query->maptype;
-
-	if($data['zoom']<10): //mapquest
-		$maptype = $data['type'];
-	else: //google
-		if($data['type'] == 'sat') $maptype = 'satellite';
-		elseif($data['type'] == 'hyb') $maptype = 'hybrid';
-	endif;
-
-	//update global mapdata
-	$mapdata = $data;
-	$mapquest = 'http://platform.beta.mapquest.com/staticmap';
-	if(is_home() or is_category() or !check_website_status($mapquest)):
-		$bufferurl =  get_bloginfo('template_url').'/images/buffer.jpg';
-		$tileurl =  get_bloginfo('template_url').'/images/tile.jpg';
-	else:
-		$mappath = $mapquest.'/v4/getmap?key=Kmjtd|luua2qu7n9,7a=o5-lzbgq&type='.$maptype.'&scalebar=false&size=1000,500';
-		$bufferurl =  $mappath.'&zoom='.($data['zoom']-1).'&center='.$data['lat'].','.$data['lng'];
-		$tileurl =  $mappath.'&zoom='.$data['zoom'].'&center='.$data['lat'].','.$data['lng']; //.$path;
-	endif;
-
-	//add the navigation controls
-	$panleft = $data['lat'].','.adjustLonByPx($data['lng'], -$xmid, $data['zoom']);
-	$panright = $data['lat'].','.adjustLonByPx($data['lng'], $xmid, $data['zoom']-1);
-	$panup = adjustLatByPx($data['lat'], $ymid/2*-1, $data['zoom']).','.$data['lng'];
-	$pandown = adjustLatByPx($data['lat'], $ymid/2*1, $data['zoom']).','.$data['lng'];
-	if($data['zoom']<15) $zoomin = $data['zoom']+2; else $zoomin = 17;
-	if($data['zoom']>3) $zoomout = $data['zoom']-2; else $zoomout = 3;
-
-	echo '<div class="navcontrols">';
-		echo '<a class="pan panleft" href="?rotate=left">&#60;</a>';
-		echo '<a class="pan panright" href="?rotate=right">&#62;</a>';
-		echo '<a class="pan panup" href="?rotate=up">&#8743;</a>';
-		echo '<a class="pan pandown" href="?rotate=down">&#8744;</a>';
-		echo '<a class="zoom zoomin" href="?zoom=in">+</a>';
-		echo '<a class="zoom zoomout" href="?zoom=out">-</a>';
-	echo '</div>';
-
+	
+	$tileurl = get_bloginfo('template_url').'/images/maps/zoom_1.jpg';
+	
 	//construct new maplayer
-	$map = '<div class="slide">';
-	$map .= '<input type="hidden" class="mapdata" value="'.implode('*',$data).'"/>';
-	$map .= '<div class="mapholder">';
-
-	$newlayer = '<div class="maplayer">';
-	$newlayer .= '<img class="buffer" src="'.$bufferurl.'" alt="">';
-	$newlayer .= '<img class="tile" src="'.$tileurl.'" alt="">';
+	$map = '<div class="slide" style="width:110%; height:110%; left: -5%; top:-5%;">';
+	$newlayer .= '<img class="mapimg" src="'.$tileurl.'" alt="" width="100%">';
 
 	//sort $posts by related count
-	if(!$postcoordinates) $markerposts = get_posts(array('cat'=>get_cat_id('Locations'), 'numberposts'=>-1));
-	else $markerposts = array($post);
-
-	foreach ($markerposts as $markerpost):
-		$linkedids = $cowobo->relations->get_related_ids($markerpost->ID);
-		$count = count($linkedids);
-		//if($count>0): //only show linked locations
-			$countarray[$markerpost->ID] = $count;
-			$linkedmarkers[] = $markerpost;
-		//endif;
-	endforeach;
-
-	if($countarray) $max = max($countarray);
-
+	$markerposts = get_posts('cat='.get_cat_id('Locations').'&numberposts=-1');
+	
 	//find marker position and add it to map
-    $id = 0;
-	foreach($linkedmarkers as $markerpost): $id++;
+    $id = 0; $xmid = 500; $ymid = 250;
+	foreach($markerposts as $markerpost): $id++;
 		$coordinates = get_post_meta($markerpost->ID, 'coordinates', true);
         if ( empty ( $coordinates ) ) continue;
 		$latlng = explode(',', $coordinates);
-		$delta_x  = (LonToX($latlng[1]) - LonToX($data['lng'])) >> (21 - $data['zoom']);
-		$delta_y  = (LatToY($latlng[0]) - LatToY($data['lat'])) >> (21 - $data['zoom']);
-   		$marker_x = ($xmid*2 + $delta_x)/($xmid*4)*100;
-   		$marker_y = ($ymid*2 + $delta_y)/($ymid*4)*100;
+		$delta_x  = (LonToX($latlng[1]) - LonToX($data['lng'])) >> (3 - $data['zoom']);
+		$delta_y  = (LatToY($latlng[0]) - LatToY($data['lat'])) >> (3 - $data['zoom']);
+   		$marker_x = ($xmid + $delta_x)/($xmid*2)*100;
+   		$marker_y = ($ymid + $delta_y)/($ymid*2)*100;
 		if($max == 0) $max = 1;
 		$percentage = $countarray[$markerpost->ID]/$max;
 		$newsize = 15 + round($percentage * 20);
 		$newmargin = '-'.($newsize/2).'px 0 0 -'.($newsize/2).'px';
 		$markerstyle = 'top:'.$marker_y.'%; left:'.$marker_x.'%; width:'.$newsize.'px; height:'.$newsize.'px; margin:'.$newmargin;
-		$marker = '<img class="marker" id="marker-'.$id.'" style="'.$markerstyle.'" src="'.get_bloginfo("template_url").'/images/mapnav.png" title="'.$coordinates.'"/>';
-		$markerlinks[] = '<a class="markerlink" id="link-'.$id.'" style="'.$markerstyle.'" href="'.get_permalink($markerpost->ID).'">'.$markerpost->post_title.'</a>';
+		$marker = '<img class="marker" style="'.$markerstyle.'" src="'.get_bloginfo("template_url").'/images/mapnav.png"/>';
+		$markerlinks[] = '<a class="markerlink" style="'.$markerstyle.'" href="'.get_permalink($markerpost->ID).'">'.$markerpost->post_title.'</a>';
 		$newlayer .= $marker;
 	endforeach;
-
-	$newlayer .= '</div>';
+	
 	$map .= $newlayer;
 	$map .= '</div>';
-	$map .= '</div>';
 
-	//now add the links to each marker in a div above the map
-	if(!$postcoordinates && $markerlinks):
-		$map .= '<div class="markerlinks"><div class="mapholder">';
+	//now add the links to a layer above the cloud mask
+	if($markerlinks):
+		$map .= '<div class="markerlinks style="width:110%"">';
 		foreach($markerlinks as $markerlink):
 			$map .= $markerlink;
 		endforeach;
-		$map .= '</div></div>';
+		$map .= '</div>';
 	endif;
 
 	return $map;
