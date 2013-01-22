f169636ceb5d33c4985a7b1a8b848ada89533e4a
codingangel <bsieders@gmail.com>
2013-01-22 11:02:26 +0200

    Added panning and resizing to map/image viewer

--- wp-content/themes/cowobo/js/script.js
+++ wp-content/themes/cowobo/js/script.js
@@ -31,29 +31,6 @@ jQuery(document).ready(function() {
 	} else {
 		jQuery('.feeds').fadeIn();
 	}
-
-
-	//Search form listerners
-	jQuery('.dropmenu input').live('click', function(event){
-		event.stopPropagation();
-		jQuery(this).parent().toggleClass('checked');
-	});
-	
-	jQuery('.dropmenu span').live('click', function(event){
-		var checkbox = jQuery(this).children('input');
-		var type = jQuery(this).parents('.dropmenu').attr('id');
-		checkbox.prop("checked", !checkbox.prop("checked"));
-		jQuery(this).toggleClass('checked');
-	});
-	
-	jQuery('.searchform').live('click', function(){
-		jQuery('.dropmenu').slideDown();
-	});
-	
-	jQuery('.closebutton').live('click', function(event){
-		event.stopPropagation();
-		jQuery('.dropmenu').slideUp();
-	});
 		
 	if(jQuery('.translating').length>0){
 		var title = jQuery(".description");
@@ -69,7 +46,141 @@ jQuery(document).ready(function() {
 		jQuery('.feeds').fadeIn();
 	}
 	
+	
+	//Enable Map Resizing and Panning
+	var pan = false; var drag = false; var previousX; var previousY;
+	
+	jQuery(".planet").mousedown(function(e) {
+		jQuery('body').addClass('unselectable');
+	    previousX = e.clientX;
+	    previousY = e.clientY;
+	    pan = true;
+	});
+	
+	jQuery(".dragbar").mousedown(function(e) {
+		jQuery('body').addClass('unselectable');
+	    previousY = e.clientY;
+	    drag = true;
+	});
+	
+	jQuery("body").mousemove(function(e) {
+	    
+		if (pan) {
+	        var slide = jQuery(".slide:last");
+			var slidepos = slide.position(); 
+			var xmax = jQuery('.planet').width()-slide.width();
+			var ymax = jQuery('.planet').height()-slide.height();
+			var ymin = -parseFloat(jQuery('.planet').css('margin-top'));
+			var newx = slidepos.left - (previousX - e.clientX);  
+			var newy = slidepos.top - (previousY - e.clientY) ;
+			if (jQuery.browser.msie) jQuery('div').attr('unselectable', 'on');
+			if(slide.find('.marker').length > 0) slide = jQuery(".slide:last, .markerlinks");
+			if(newx > 0) newx = 0;
+			if(newx < xmax) newx = xmax;
+			if(newy > ymin) newy = ymin;
+			if(newy < ymax) newy = ymax;
+			slide.css({top: newy, left: newx});
+	        previousX = e.clientX;
+	        previousY = e.clientY;
+	    }
+		
+		if (drag) {
+	        var planet = jQuery(".planet");
+			var planetpos = parseFloat(planet.css('margin-top'));
+			var slide = jQuery('.slide:last');
+			var slidepos = slide.position();
+			var mousemove = previousY - e.clientY;
+			var newy = planetpos - mousemove;
+			var overlap = slidepos.top + (slide.height() - planet.height());
+			if(overlap < 0) mousemove = 0;
+			if(newy > 0) newy = 0;
+			if(newy < -300) newy = -300;
+			planet.css('margin-top', newy);
+			slide.css('top', slidepos.top + mousemove);
+	        previousY = e.clientY;
+	    }
+	});
+	
+	//disable panning and dragging on mouse up
+	jQuery(document).mouseup(function() {
+	    pan = false; drag = false;
+		jQuery('body').removeClass('unselectable');
+	});
+	
 });
+
+//IMAGE VIEWER FUNCTIONS//
+jQuery('.zoom, .pan, .labels').live('click', function(event){
+	event.preventDefault();
+	var action = jQuery(this).attr('class').split(' ')[1];
+	var slide = jQuery('.slide:last');
+	var slideimg = jQuery('.slide .mapimg');
+	var slidepos = slide.position();
+	var xmax = jQuery('.planet').width()-slide.width();
+	var ymax = jQuery('.planet').height()-slide.height();
+	var amount; var newstyle;
+	
+	if(action == 'labels') {
+		//to do: change day/night maptype
+	} else if(action == 'panleft') {
+		if(slidepos.left < -300) amount = slidepos.left + 300; else amount = 0;
+		newstyle = {left: amount}
+	} else if(action == 'panright') {
+		if(slidepos.left > xmax + 300) amount = slidepos.left - 300; else amount = xmax;
+		newstyle = {left: amount}
+	} else if(action == 'panup') {
+		if(slidepos.top < -200) amount = slidepos.top + 200; else amount = 0;
+		newstyle = {top: amount}
+	} else if(action == 'pandown') {
+		if(slidepos.top > ymax + 200) amount = slidepos.top - 200; else amount = ymax;
+		newstyle = {top: amount}
+	} else if(action == 'zoomin') {
+		var curzoom = parseFloat(slide[0].style.width.split('%')[0]);
+		if(curzoom < 400) {
+			var newzoom = curzoom * 2 + '%';
+			var newtop = slidepos.top - (slide.height()/2) + 'px';
+			var newleft = slidepos.left - (slide.width()/2) + 'px';
+			var imgname = slideimg.attr('src').split('_')[0];
+			newstyle = {width:newzoom, height:newzoom, top:newtop, left:newleft};
+			slideimg.clone().appendTo(slide).attr('src', imgname + '_3.jpg');
+		}
+	} else if(action ==  'zoomout') {
+		newstyle = {width:"100%", height:"100%", top:"0", left:"0"};
+	}
+	
+	//if slide contains markers than animate that too
+	if(slide.find('.marker').length > 0) slide = jQuery(".slide:last, .markerlinks");
+	slide.animate(newstyle, 2000);
+});
+
+
+//expand map to max function
+function expand_map() {
+    return null;
+}
+
+//Search form listerners
+jQuery('.dropmenu input').live('click', function(event){
+	event.stopPropagation();
+	jQuery(this).parent().toggleClass('checked');
+});
+	
+jQuery('.dropmenu span').live('click', function(event){
+	var checkbox = jQuery(this).children('input');
+	var type = jQuery(this).parents('.dropmenu').attr('id');
+	checkbox.prop("checked", !checkbox.prop("checked"));
+	jQuery(this).toggleClass('checked');
+});
+	
+jQuery('.searchform').live('click', function(){
+	jQuery('.dropmenu').slideDown();
+});
+	
+jQuery('.closebutton').live('click', function(event){
+	event.stopPropagation();
+	jQuery('.dropmenu').slideUp();
+});
+	
 
 /// TAB FUNCTIONS ///
 jQuery(document).ready(function($) {
@@ -95,182 +206,14 @@ jQuery('.hidemap').live('click', function(event) {
 });
 
 //show specific slide in gallery
-jQuery('.galthumbs a').live('click', function(event) {
-	var oldnum = jQuery('.gallery .slide:last').attr('id').split('-')[1];
-	var oldthumb = jQuery('.galthumbs a').eq(oldnum);
-	var newnum = jQuery(this).index();
+jQuery('.fourths a').live('click', function(event) {
+	var num = jQuery(this).index();
+	var slide = jQuery('#slide-'+num);
+	alert(num);
 	event.preventDefault();
-	if(jQuery(this).children('img').length>0){
-		jQuery('#slide-'+newnum).hide().appendTo(jQuery('.gallery')).fadeIn(1000);
-		jQuery(this).hide();
-		oldthumb.show();
-	}
+	slide.hide().appendTo(jQuery('.planet')).fadeIn(1000);
 });
 
-
-//MAP FUNCTIONS//
-
-var offset = 268435456; // center of google map in pixels at max zoom level
-function LonToX(lon) {
-	return Math.round(offset + (offset / Math.PI) * lon * Math.PI / 180);
-}
-function LatToY(lat) {
-	return Math.round(offset - (offset / Math.PI) * Math.log((1 + Math.sin(lat * Math.PI / 180)) / (1 - Math.sin(lat * Math.PI / 180))) / 2);
-}
-function XToLon(x) {
-	return ((Math.round(x) - offset) / (offset / Math.PI)) * 180/ Math.PI;
-}
-function YToLat(y) {
-	return (Math.PI / 2 - 2 * Math.atan(Math.exp((Math.round(y) - offset) / (offset/ Math.PI)))) * 180 / Math.PI;
-}
-function adjustLonByPx(lon, amount, zoom) {
-	var newlon = XToLon(LonToX(lon) + (amount << (21 - zoom)));
-	if (newlon < -180) newlon = 360 + newlon;
-	else if (newlon > 180) newlon = newlon - 360;
-	return newlon;
-}
-function adjustLatByPx(lat, amount, zoom) {
-	var newlat = YToLat(LatToY(lat) + (amount << (21 - zoom)));
-	if (newlat < -90) newlat = 180 + newlat;
-	else if (newlat > 90) newlat = newlat - 180;
-	return newlat;
-}
-
-jQuery('.zoom, .pan, .labels').live('click', function(event){
-	event.preventDefault();
-	clearInterval(slideshow);
-	jQuery('.pauseshow').hide().next().show();
-
-	var xmid = 500; var ymid = 250;
-	var action = jQuery(this).attr('class').split(' ')[1];
-
-	//only start animating new map if the old map has finished loading
-	if(animate == false){
-		//update global parameters
-		if(action == 'labels') {
-			var newstyle = 'labels';
-			if(data.type == 'hyb'){
-				data.type = 'sat';
-				jQuery(this).removeClass('grey');
-			} else {
-				data.type = 'hyb';
-				jQuery(this).addClass('grey');
-			}
-		} else if(action == 'panleft') {
-			data.lng = adjustLonByPx(data.lng, -xmid, data.zoom);
-			var newstyle = {left:'25%'}
-		} else if(action == 'panright') {
-			data.lng = adjustLonByPx(data.lng, xmid, data.zoom);
-			var newstyle = {left:"-25%"};
-		} else if(action == 'panup') {
-			data.lat = adjustLatByPx(data.lat, -ymid, data.zoom);
-			var newstyle = {top:'25%'};
-		} else if(action == 'pandown') {
-			data.lat = adjustLatByPx(data.lat, ymid, data.zoom);
-			var newstyle = {top:"-25%"};
-		} else if(action == 'zoomin') {
-			if(data.zoom < 15) {
-				data.zoom = parseFloat(data.zoom)+1;
-				var newstyle = {width:"200%", height:"200%", top:"-50%", left:"-50%"};
-			} else {
-				var newstyle = '';
-			}
-		} else if(action ==  'zoomout') {
-			if(data.zoom > 3) {
-				data.zoom = parseFloat(data.zoom)-1;
-				var newstyle = {width:"50%", height:"50%", top:"25%", left:"25%"};
-			} else {
-				var newstyle = '';
-			}
-		}
-
-		if(newstyle != '') loadnewmap(newstyle);
-	}
-});
-
-function loadnewmap(newstyle) {
-	var xmid = 500; var ymid = 250;
-	var currlayer = jQuery('.maplayer:first');
-	var path = '';
-	animate = true;
-	if(typeof(data.path)!= 'undefined') path ='&path=weight:2%7Ccolor:0xffffffff%7Cenc:'+data.path;
-
-	if(data.zoom<10){
-		var mappath = 'http://platform.beta.mapquest.com/staticmap/v4/getmap?key=Kmjtd|luua2qu7n9,7a=o5-lzbgq&type='+data.type+'&scalebar=false&size=1000,500';
-	} else {
-		if(data.type = 'sat') var maptype = 'satellite'; else if(data.type = 'hyb') var maptype = 'hybrid';
-		var mappath = 'http://maps.googleapis.com/maps/api/staticmap?maptype='+maptype+'&sensor=false&format=jpg&size=1000x500';
-	}
-	var bufferurl = mappath+'&zoom='+(data.zoom-1)+'&center='+data.lat+','+data.lng;
-	var tileurl = mappath+'&zoom='+data.zoom+'&center='+data.lat+','+data.lng;
-
-	var newlayer = jQuery('<div class="maplayer"><img class="buffer" src="'+bufferurl+'" alt=""><img class="tile" src="'+tileurl+'" alt=""></div>');
-
-	jQuery('.maploading').show();
-
-	//animage globe
-	if(newstyle == 'labels') {
-		newlayer.hide().children('.tile').load(function() { animate=false;
-			newlayer.insertAfter(currlayer).fadeIn(2000, function(){
-				currlayer.hide().appendTo(newlayer).children('.marker').appendTo(newlayer);
-			});
-			jQuery('.maploading').hide();
-		});
-	} else {
-		currlayer.animate(newstyle, 2000, function() { animate=false;
-			//add newlayer and append current so they can be animated together
-			newlayer.insertAfter(currlayer).append(currlayer);
-
-			//hide old layer when buffer has finished loading
-			newlayer.children('.buffer').load(function() {
-				jQuery('.maploading').hide();
-				currlayer.hide();
-			});
-
-			//move markers to positions on newlayer
-			currlayer.children('.marker').each(function(){
-				var marker = jQuery(this);
-				var markerid = jQuery(this).attr('id').split('-')[1];
-				var markerpos = jQuery(this).attr('title').split(',');
-				var markerlink = jQuery('#link-'+markerid);
-				var delta_x  = (LonToX(markerpos[1]) - LonToX(data.lng)) >> (21 - data.zoom);
-				var delta_y  = (LatToY(markerpos[0]) - LatToY(data.lat)) >> (21 - data.zoom);
-		   		var marker_x = ((xmid*2 + delta_x)/(xmid*4)*100)+'%';
-		   		var marker_y = ((ymid*2 + delta_y)/(ymid*4)*100)+'%';
-				marker.hide().css({top:marker_y, left: marker_x});
-				markerlink.css({top:marker_y, left: marker_x});
-				marker.appendTo(newlayer).show();
-			});
-		});
-	}
-
-}
-
-//animate zooming of map to location of mouse
-jQuery('.planet').live('click', function(e){
-	var currslide = jQuery('.slide:visible').last();
-	if(currslide.children('.mapholder').length>0){
-		if(animate == false && data.zoom < 16){
-			var mousex = e.clientX-jQuery(this).offset().left;
-			var mousey = e.clientY;
-			var xpercent = 	mousex/1000;
-			var ypercent = mousey/500;
-			data.lat = adjustLatByPx(data.lat, mousey - 250, data.zoom);
-			data.lng = adjustLonByPx(data.lng, mousex - 500, data.zoom);
-			data.zoom = parseFloat(data.zoom)+1;
-			var newstyle = {width:"200%", height:"200%", top: (-ypercent*100) + "%", left: (-xpercent*100) + "%"};
-			loadnewmap(newstyle);
-		}
-	} else if (currslide.children('object').length>0) {
-		//play youtube video
-		var player = currslide.find('embed');
-		player.attr("src", player.attr("src")+'?autoplay=1');
-	}
-});
-
-jQuery('.planet a').live('click', function(e){
-	e.stopPropagation();
-});
 
 //TEXT EDITOR FUNCTIONS
 
