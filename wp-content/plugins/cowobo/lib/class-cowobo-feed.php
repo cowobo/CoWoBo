28825545623b19f3072796fe40df7076d0fba98a
Mike <mike@trenvo.com>
2013-01-24 22:46:05 +0200

    Uninitialized variable

--- wp-content/plugins/cowobo/lib/class-cowobo-feed.php
+++ wp-content/plugins/cowobo/lib/class-cowobo-feed.php
@@ -11,7 +11,7 @@ class CoWoBo_Feed
      * @todo When is this method really called?
      */
     public function filter_feed(){
-        
+
 
         //store variables from browse form
         $cats = cowobo()->query->cats;
@@ -25,6 +25,7 @@ class CoWoBo_Feed
 
 
 		//todo: handle multiple sort values
+        $metaquery = array();
 		$sort = $sortby[0];
         $direction = '';
         if ( empty ( $sort ) ) $sort = 'modified';
@@ -60,7 +61,7 @@ class CoWoBo_Feed
      */
     function feed_title(){
         global $currentcat, $post, $cowobo;
-		
+
         $feedtitle = '';
         if( cowobo()->query->new )
             $feedtitle .= 'Add '.cowobo()->query->new;
@@ -80,9 +81,9 @@ class CoWoBo_Feed
             $feedtitle = 'Change Language';
         elseif( cowobo()->query->action == 'editpost')
             $feedtitle = 'Edit Post';
-		elseif( cowobo()->users->is_profile() ) 
+		elseif( cowobo()->users->is_profile() )
 			$feedtitle = $post->post_title;
-        elseif( is_single() or is_category()) 
+        elseif( is_single() or is_category())
             $feedtitle = $currentcat->name;
         elseif( is_home() )
             $feedtitle = 'Welcome!';
