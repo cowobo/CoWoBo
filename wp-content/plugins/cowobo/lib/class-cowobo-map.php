8ebf395e2f43ef649a37cf356518d8d4f8902d31
codingangel <bsieders@gmail.com>
2013-01-24 14:42:00 +0200

    Improved zooming and panning after resizing window

--- wp-content/plugins/cowobo/lib/class-cowobo-map.php
+++ wp-content/plugins/cowobo/lib/class-cowobo-map.php
@@ -100,27 +100,52 @@ function cwb_loadmap() {
 	$linkedmarkers = array();
 	
 	$data = array('lat'=> '20', 'lng'=>'0', 'zoom'=>1);
-
+	$zoom1src = get_bloginfo('template_url').'/images/maps/zoom_2.jpg';
+	$zoom2src = get_bloginfo('template_url').'/images/maps/zoom_3.jpg';
+		
 	//get coordinates if specified in url or post
 	if(is_single()):
 		if( cowobo()->query->post_ID ) $postid = cowobo()->query->post_ID;
 		else $postid = $post->ID;
 		$postcoordinates = get_post_meta($postid, 'coordinates', true);
-		$tileurl = get_bloginfo('template_url').'/images/maps/zoom_1.jpg';
+		$zoomlevel = 1;
+	else:
+		$zoomlevel = 0;
 	endif;
 	
-	$tileurl = get_bloginfo('template_url').'/images/maps/zoom_1.jpg';
-	
 	//construct new maplayer
-	$map = '<div class="slide zoom1" id="slide-0" style="width:110%">';
-	$newlayer .= '<img class="mapimg" src="'.$tileurl.'" alt="" width="100%">';
+	$map = '<div class="slide zoom'.$zoomlevel.'" id="slide-0">';
+	$newlayer .= '<img class="slideimg" src="'.$zoom1src.'" alt="" width="100% height="100%">';
+	$newlayer .= '<input type="hidden" class="zoomlevel" value="'.$zoomlevel.'"/>';
+	$newlayer .= '<input type="hidden" class="zoomsrc2" value="'.$zoom2src.'"/>';
 
 	//sort $posts by related count
-	$markerposts = get_posts('cat='.get_cat_id('Locations').'&numberposts=-1');
-	
+	if(is_search() or is_category() && have_posts()){
+		while (have_posts()) : the_post();
+			if($coordinates = get_post_meta($post->ID, 'coordinates', true)){
+				$linkedids = $cowobo->relations->get_related_ids($post->ID);
+				$count = count($linkedids);
+				$countarray[$post->ID] = $count;
+				$linkedmarkers[] = $post;
+			}
+		endwhile;
+	} else {
+		$markerposts = get_posts('cat='.get_cat_id('Locations').'&numberposts=-1');
+		foreach ($markerposts as $markerpost):
+			$linkedids = $cowobo->relations->get_related_ids($markerpost->ID);
+			$count = count($linkedids);
+			$countarray[$markerpost->ID] = $count;
+			$linkedmarkers[] = $markerpost;
+		endforeach;
+	}
+
+	//store the maximum number of links
+	if($countarray) $max = max($countarray);
+	if($max == 0) $max = 1;
+
 	//find marker position and add it to map
     $id = 0; $xmid = 500; $ymid = 250;
-	foreach($markerposts as $markerpost): $id++;
+	foreach($linkedmarkers as $markerpost): $id++;
 		$coordinates = get_post_meta($markerpost->ID, 'coordinates', true);
         if ( empty ( $coordinates ) ) continue;
 		$latlng = explode(',', $coordinates);
@@ -143,7 +168,7 @@ function cwb_loadmap() {
 
 	//now add the links to a layer above the cloud mask
 	if($markerlinks):
-		$map .= '<div class="markerlinks zoom1">';
+		$map .= '<div class="markerlinks zoom'.$zoomlevel.'">';
 		foreach($markerlinks as $markerlink):
 			$map .= $markerlink;
 		endforeach;
