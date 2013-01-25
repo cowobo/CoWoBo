27f82ee820495946800ea743b5629036271e7420
codingangel <bsieders@gmail.com>
2013-01-25 20:08:18 +0200

    Fixed page up bug on contenteditable divs

--- wp-content/themes/cowobo/index.php
+++ wp-content/themes/cowobo/index.php
@@ -145,7 +145,7 @@ else:
 				echo '<div class="rownumbers">';
 					for($x=1; $x<300; $x++): echo $x.'<br/>'; endfor;
 				echo '</div>';
-					echo '<div class="notranslate code">'.htmlentities(file_get_contents(TEMPLATEPATH.'/templates/pagesource.php')).'</div>';
+					echo '<div class="notranslate code"><pre>'.htmlentities(file_get_contents(TEMPLATEPATH.'/templates/pagesource.php')).'</pre></div>';
 			echo '</div>';
 		echo '</div>';
 
