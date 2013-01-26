371c08354bde09c0eb8dda3b2ee4a9f8e0c5f275
codingangel <bsieders@gmail.com>
2013-01-26 15:07:47 +0200

    Added rich text editor to contact form

--- wp-content/themes/cowobo/templates/contact.php
+++ wp-content/themes/cowobo/templates/contact.php
@@ -2,16 +2,24 @@
 
 
 echo '<div class="tab">';
-	echo '<h2>Send us an email &raquo;</h2>';
 	if( cowobo()->query->user_email ) echo 'Your message has been sent.';
 	echo '<form method="post" action="">';
-		echo '<textarea name="emailtext" rows="3" class="emailtext"></textarea>';
+		echo '<div class="richbuttons">';
+			echo '<a class="makebold" href="#">Bold</a>';
+			echo '<a class="makeitalic" href="#">Italic</a>';
+			echo '<a class="makeunderline" href="#">Underline</a>';
+			echo '<a class="makelink" href="#">Link</a>';
+			echo '<a class="htmlmode" href="#">HTML</a>';
+			echo '<a class="richmode" href="#">WYSIWYG</a>';
+		echo '</div>';
+		echo '<div id="rte" contenteditable="true" unselectable="off" tabindex="'.$index.'" class="richtext">'.trim ( $post_content ).'</div>';
+		echo '<textarea name="emailtext" rows="12" class="htmlbox"></textarea>';		
 		echo '<input type="text" name="user" class="hide" value=""/>'; //spammer trap
-		echo '<input type="input" class="half" name="user_firstname" value="Your Name" onfocus="this.value=\'\'"/>';
-		echo '<input type="input" class="half" name="user_email" value="Your Email" onfocus="this.value=\'\'"/>';
+		echo '<input type="input" class="lefthalf" name="user_firstname" value="Your Name" onfocus="this.value=\'\'"/>';
+		echo '<input type="input" class="righthalf" name="user_email" value="Your Email" onfocus="this.value=\'\'"/>';
 		echo '<div class="clear" style="margin-top:5px">';
         wp_nonce_field( 'sendemail', 'sendemail' );
-		echo '<button type="submit" class="button">Send Email</button>';
+		echo '<button type="submit" class="button submitform">Send Email</button>';
 		echo '</div>';
 	echo '</form>';
 echo '</div>';
