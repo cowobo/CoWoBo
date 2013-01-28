2ba0761c9699350ae9888c2439a3b5fc36ce3522
Mike <mike@trenvo.com>
2013-01-28 18:55:04 +0200

    Added first widgets.php

---
+++ wp-content/plugins/cowobo/lib/widgets.php
@@ -0,0 +1,28 @@
+<?php
+add_action('widgets_init', 'cowobo_register_widgets' );
+function cowobo_register_widgets() {
+    register_widget('cowobo_profile_widget');
+}
+
+class CoWoBo_Profile_Widget extends WP_Widget {
+
+    public function __construct() {
+        parent::WP_Widget('cowobo_profile_widget', 'CoWoBo Profile', array('description' => 'Display the CoWoBo Profile Badge' ) );
+    }
+
+        public function cowobo_profile_widget() {
+            $this->__construct();
+        }
+
+    public function widget($args, $instance) {
+        extract($args, EXTR_SKIP);
+
+        echo $before_widget;
+        //echo $before_title . "Your Profile" . $after_title;
+        do_action ( 'cowobo_profile_widget' );
+        echo $after_widget;
+    }
+
+
+
+}
