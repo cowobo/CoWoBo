2fa6bb15315edd6cb03ae3a474b91a7eabe413fc
Mike <mike@trenvo.com>
2013-01-30 19:42:33 +0200

    Don't show login link when logging in

--- wp-content/themes/cowobo/templates/widgets/tour.php
+++ wp-content/themes/cowobo/templates/widgets/tour.php
@@ -1,8 +1,11 @@
 <?php
 echo "<h2>Welcome to Coders Without Borders!</h2>";
 echo "<p>Is this your first time? <a href='#'>Take the tour!</a></p>";
-echo "<p>Returning visitor? <a href='/?action=login' class='toggle-loginform'>Login</a></p>";
 
+if ( cowobo()->query->action == 'login' ) return;
+
+echo "<p>Returning visitor? <a href='/?action=login' class='toggle-loginform'>Login</a></p>";
 echo "<div class='loginform hide-if-js'>";
+cowobo()->query->login = 'login';
 include TEMPLATEPATH . '/templates/login.php';
 echo "</div>";
