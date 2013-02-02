adc2c8ef3a7e1e3e36dd0955d9f553096b10f53e
codingangel <bsieders@gmail.com>
2013-02-02 14:40:24 +0200

    Added recent posts to 404 page

--- wp-content/themes/cowobo/templates/404.php
+++ wp-content/themes/cowobo/templates/404.php
@@ -1,7 +1,14 @@
 <?php
 
 echo '<div class="tab center">';
-	echo 'Click your back button and try again';
+	echo '<h2>Could it be one of these?</h2>';
 echo '</div>';
+
+//todo query related posts to missing url
+query_posts('numberposts=8');
+if (have_posts()): while (have_posts()) : the_post();
+	$tabtype = 'post'; include(TEMPLATEPATH.'/templates/tabs.php');
+endwhile; endif;
+
 
 ?>
