6feb00e464c412407b70b703251b0583facbba21
codingangel <bsieders@gmail.com>
2013-02-05 12:15:14 +0200

    Added Custom map position to location posts

--- wp-content/plugins/cowobo/lib/class-cowobo-layouts.php
+++ wp-content/plugins/cowobo/lib/class-cowobo-layouts.php
@@ -44,8 +44,8 @@ class Cowobo_Layouts {
 		$this->layout[get_cat_ID('Wikis')] = array(
 			array('type' => 'title', 'label' =>'Title', 'hint' => 'Keep it short and sweet'),
 			array('type' => 'gallery', 'label' =>'Gallery', 'hint' => 'Add Images and Videos (leave blank to delete)'),
-			array('type' => 'tags', 'label' =>'Tags', 'hint' => 'Separate tags with commas ","'),
-			array('type' => 'location', 'label' =>'Location', 'hint' => 'Enter nearest city and country'),
+			array('type' => 'categories', 'label' =>'Category', 'hint' => 'Select up to three relevant categories'),
+			array('type' => 'location', 'label' =>'Location', 'hint' => 'Optional: enter a city or address'),
 			array('type' => 'website', 'label' =>'Source', 'hint' => 'ie http://www.wikipedia.org/cowobo'),
 			array('type' => 'largetext', 'label' =>'Outline', 'hint' => 'Max 3000 characters'),
 		);
@@ -53,8 +53,7 @@ class Cowobo_Layouts {
 		$this->layout[get_cat_ID('News')] = array(
 			array('type' => 'title', 'label' =>'Title', 'hint' => 'Keep it short and sweet'),
 			array('type' => 'gallery', 'label' =>'Gallery', 'hint' => 'Add Images and Videos (leave blank to delete)'),
-			array('type' => 'tags', 'label' =>'Tags', 'hint' => 'Separate tags with commas ","'),
-			array('type' => 'location', 'label' =>'Location', 'hint' => 'Enter nearest city and country'),
+			array('type' => 'location', 'label' =>'Location', 'hint' => 'Enter a city or address'),
 			array('type' => 'website', 'label' =>'Source', 'hint' => 'ie http://www.wikipedia.org/cowobo'),
 			array('type' => 'largetext', 'label' =>'In Brief', 'hint' => 'Max 3000 characters'),
 		);
@@ -62,8 +61,7 @@ class Cowobo_Layouts {
 		$this->layout[get_cat_ID('Projects')] = array(
 			array('type' => 'title', 'label' =>'Project Name', 'hint' => 'Keep it short and sweet'),
 			array('type' => 'gallery', 'label' =>'Gallery', 'hint' => 'Add Images and Videos (leave blank to delete)'),
-			array('type' => 'tags', 'label' =>'Tags', 'hint' => 'Separate tags with commas ","'),
-			array('type' => 'location', 'label' =>'Location', 'hint' => '(Optional) Select the city nearest to the project'),
+			array('type' => 'location', 'label' =>'Location', 'hint' => 'Optional: enter a city or address'),
 			array('type' => 'involvement', 'label' =>'Involvement', 'hint' => 'Select your role in the project'),
 			array('type' => 'dropdown', 'label' =>'Status', 'hint' => 'Completed,Prototype,Under Construction'),
 			array('type' => 'website', 'label' =>'Website', 'hint' => 'ie http://www.wikipedia.org/cowobo'),
@@ -75,7 +73,7 @@ class Cowobo_Layouts {
 			array('type' => 'title', 'label' =>'Full Name', 'hint' => 'Keep it real if you can'),
 			array('type' => 'gallery', 'label' =>'Gallery', 'hint' => 'Add Images and Videos (leave blank to delete)'),
 			array('type' => 'tags', 'label' =>'Tags', 'hint' => 'Separate tags with commas ","'),
-			array('type' => 'location', 'label' =>'Current Location', 'hint' => 'Enter nearest city and country'),
+			array('type' => 'location', 'label' =>'Current Location', 'hint' => 'Enter your city or address'),
 			array('type' => 'checkboxes', 'label' =>'Coding Languages', 'hint' => 'C++,C#,Html,Java,jQuery,PHP,Perl,Python,Ruby,Visual Basic'),
 			array('type' => 'website', 'label' =>'Website', 'hint' => 'ie http://www.myblog.com'),
 			array('type' => 'smalltext', 'label' =>'Looking For', 'hint' => 'ie Collaborators, Funding, etc'),
@@ -96,7 +94,7 @@ class Cowobo_Layouts {
 			array('type' => 'gallery', 'label' =>'Gallery', 'hint' => 'Add Images and Videos (leave blank to delete)'),
 			array('type' => 'tags', 'label' =>'Tags', 'hint' => 'Separate tags with commas ","'),
 			array('type' => 'dates', 'label' =>'Dates', 'hint' => 'Please use the format dd/mm/yyyy'),
-			array('type' => 'location', 'label' =>'Location', 'hint' => 'Enter nearest city and country'),
+			array('type' => 'location', 'label' =>'Location', 'hint' => 'Enter the complete address'),
 			array('type' => 'smalltext', 'label' =>'Address', 'hint' => 'Street address of event'),
 			array('type' => 'smalltext', 'label' =>'Contact Info', 'hint' => 'ie tel, email'),
 			array('type' => 'website', 'label' =>'Website', 'hint' => 'ie http://www.wikipedia.org/cowobo'),
@@ -107,7 +105,7 @@ class Cowobo_Layouts {
 			array('type' => 'title', 'label' =>'Title', 'hint' => 'Keep it short and sweet'),
 			array('type' => 'gallery', 'label' =>'Gallery', 'hint' => 'Add Images and Videos (leave blank to delete)'),
 			array('type' => 'tags', 'label' =>'Tags', 'hint' => 'Separate tags with commas ","'),
-			array('type' => 'location', 'label' =>'Location', 'hint' => 'Enter nearest city and country'),
+			array('type' => 'location', 'label' =>'Location', 'hint' => 'Optional: enter a city or address'),
 			array('type' => 'smalltext', 'label' =>'Contact Info', 'hint' => 'Will only be visible to logged in users'),
 			array('type' => 'website', 'label' =>'Website', 'hint' => 'ie http://www.wikipedia.org/cowobo'),
 			array('type' => 'largetext', 'label' =>'Description', 'hint' => 'Max 3000 characters'),
@@ -116,6 +114,7 @@ class Cowobo_Layouts {
 		$this->layout[get_cat_ID('Locations')] = array(
 			array('type' => 'title', 'label' =>'Name of location', 'hint' => 'Make sure it does not already exist on our site'),
 			array('type' => 'country', 'label' =>'Country', 'hint' => 'Enter the correct English spelling'),			
+			array('type' => 'location', 'label' =>'Map position', 'hint' => 'Optional: Enter a custom address to center the map'),
 			array('type' => 'gallery', 'label' =>'Gallery', 'hint' => 'Add Images and Videos (leave blank to delete)'),
 			array('type' => 'largetext', 'label' =>'Description', 'hint' => 'Keep it short and sweet and sweet'),
 		);
@@ -123,8 +122,7 @@ class Cowobo_Layouts {
 		$this->layout[get_cat_ID('Blogs')] = array(
 			array('type' => 'title', 'label' =>'Title', 'hint' => 'Keep it short and sweet'),
 			array('type' => 'gallery', 'label' =>'Gallery', 'hint' => 'Add Images and Videos (leave blank to delete)'),
-			array('type' => 'tags', 'label' =>'Tags', 'hint' => 'Separate tags with commas ","'),
-			array('type' => 'location', 'label' =>'Location', 'hint' => 'Enter nearest city and country'),
+			array('type' => 'location', 'label' =>'Location', 'hint' => 'Optional: enter a city or address'),			
 			array('type' => 'largetext', 'label' =>'Blog Text', 'hint' => 'Max 3000 characters'),
 		);
 
@@ -132,7 +130,7 @@ class Cowobo_Layouts {
 			array('type' => 'title', 'label' =>'Name of Partner', 'hint' => 'Keep it short and sweet'),
 			array('type' => 'gallery', 'label' =>'Gallery', 'hint' => 'Add Images and Videos (leave blank to delete)'),
 			array('type' => 'tags', 'label' =>'Tags', 'hint' => 'Separate tags with commas ","'),
-			array('type' => 'location', 'label' =>'Location', 'hint' => 'Enter nearest city and country'),
+			array('type' => 'location', 'label' =>'Map Position', 'hint' => 'Optional: enter a city or address'),
 			array('type' => 'slogan', 'label' =>'Slogan', 'hint' => 'One line that helps describe the project'),
 			array('type' => 'largetext', 'label' =>'Description', 'hint' => 'Max 3000 characters'),
 		);
